c7badf535bede637cacb3acc505c6bfcede2f73a
nedate1976 <ndate1976@hotmail.com>
2024-01-13 14:30:53 +0530

    initial commit

---
+++ src/main/java/com/saadhak/app/configserver/SaadhakConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.saadhak.app.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaadhakConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaadhakConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadhak/app/configserver/SaadhakConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadhak.app.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaadhakConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
